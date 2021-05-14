244aec558e93d3c89fe5ad2521c769e24a00cb0d
omarsagoo <om.sagoo@gmail.com>
2021-05-14 16:38:14 -0700

    initialize new channel

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const io = require('socket.io')(server);
 //We'll store our online users here
 let onlineUsers = {};
 io.on("connection", (socket) => {
-  // Make sure to send the users to our chat file
-  require('./sockets/chat.js')(io, socket, onlineUsers);
+    // Make sure to send the users to our chat file
+    require('./sockets/chat.js')(io, socket, onlineUsers);
 })
 //app.js
 //Express View Engine for Handlebars
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 //Establish your public folder
 app.use('/public', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 })
 
 server.listen('3000', () => {
-  console.log('Server listening on Port 3000');
+    console.log('Server listening on Port 3000');
 })
